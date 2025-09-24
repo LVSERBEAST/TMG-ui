2876a2cea0b8a6d43a080325ca2093f1cede853d
laserbeast <mpmarkgraf@gmail.com>
2025-09-24 01:00:57 -0400

    Project setup

---
+++ src/app/features/pages/components/page-form/page-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-form',
+  imports: [],
+  templateUrl: './page-form.html',
+  styleUrl: './page-form.scss'
+})
+export class PageForm {
+
+}
---
+++ src/app/features/pages/components/page-list/page-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-list',
+  imports: [],
+  templateUrl: './page-list.html',
+  styleUrl: './page-list.scss'
+})
+export class PageList {
+
+}
